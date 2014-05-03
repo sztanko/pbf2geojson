bc19121f8553539adb6a2af76f4b8f10f68137bc
Demeter Sztanko <Demeter Sztanko>
2014-05-03 20:39:41 +0100

    [master]: Added some description to wrap.py

---
+++ wrap.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+import json
+
+#Purpose of this scirpt is to make a single geoJSON out of file that contains a separate geojson file in every line.
+
+out={}
+out['type']='FeatureCollection'
+out['features']=[]
+
+c=1;
+for line in sys.stdin:
+    j=json.loads(line)
+    f={'type': 'Feature'}
+    f['properties']=j['properties']
+    f['id']=c
+    j['properties']=None
+    del j['properties']
+    f['geometry']=j
+    out['features'].append(f)
+    c+=1
+out['properties']={'count':len(out)}
+print json.dumps(out)
